bd68ec0c5f5861812639123105bef0049a2cd5fd
Calle Sönne <csonne@google.com>
2025-05-18 21:01:00 +0100

    Formalize the missing `imo_1979_p6`
    PiperOrigin-RevId: 760343274

--- Imo/1979/P6.lean
+++ Imo/1979/P6.lean
@@ -21,6 +21,45 @@ open scoped Nat
 
 /--
 Let $A$ and $E$ be opposite vertices of an octagon. A frog starts at vertex $A$. From any vertex except $E$ it jumps to one of the two adjacent vertices. When it reaches $E$ it stops. Let $a_n$ be the number of distinct paths of exactly $n$ jumps ending at $E$. Prove that: $a_{2n-1}=0,a_{2n}={(2+\sqrt2)^{n-1}\over\sqrt2}-{(2-\sqrt2)^{n-1}\over\sqrt2}$. -/
-@[imo_problem_subject combinatorics]
-theorem imo_1979_p6 : (sorry : Prop) := by
+theorem imo_1979_p6.parts.odd
+    -- We label the vertices of an octagon by `Fin 8`.
+    -- (Let's also say for later that jumping 0 -> 1 -> 2 -> .. is in the clockwise direction)
+    (A E : Fin 8)
+    -- `E` is the opposite of `A`
+    (hE : E = A + 4)
+    -- Let `a` be the function such that `a n` is the number of distinct sequences of `n` jumps
+    -- ending exactly at `E`.
+    (a : ℕ → ℕ)
+    -- We formulate a path as a function `Fin n → Fin 2`, where `p i = 0` means that the `i+1`th
+    -- jump is clockwise.
+    (ha_def : ∀ n, a n =
+      Fintype.card {p : Fin n → ℤˣ // A + ∑ i, (p i : Fin 8) = E})
+    (n : ℕ) (hn : 0 < n) :
+    a (2*n - 1 : ℕ) = 0 := by
   sorry
+
+/--
+Let $A$ and $E$ be opposite vertices of an octagon. A frog starts at vertex $A$. From any vertex except $E$ it jumps to one of the two adjacent vertices. When it reaches $E$ it stops. Let $a_n$ be the number of distinct paths of exactly $n$ jumps ending at $E$. Prove that: $a_{2n-1}=0,a_{2n}={(2+\sqrt2)^{n-1}\over\sqrt2}-{(2-\sqrt2)^{n-1}\over\sqrt2}$. -/
+theorem imo_1979_p6.parts.even
+    (A E : Fin 8)
+    (hE : E = A + 4)
+    (a : ℕ → ℕ)
+    (ha_def : ∀ n, a n =
+      Fintype.card {p : Fin n → ℤˣ // A + ∑ i, (p i : Fin 8) = E})
+    (n : ℕ) (hn : 0 < n) :
+    a (2 * n) = (2 + √2)^(n - 1 : ℕ) / √2 - (2 - √2)^(n - 1 : ℕ) / √2 := by
+  sorry
+
+/--
+Let $A$ and $E$ be opposite vertices of an octagon. A frog starts at vertex $A$. From any vertex except $E$ it jumps to one of the two adjacent vertices. When it reaches $E$ it stops. Let $a_n$ be the number of distinct paths of exactly $n$ jumps ending at $E$. Prove that: $a_{2n-1}=0,a_{2n}={(2+\sqrt2)^{n-1}\over\sqrt2}-{(2-\sqrt2)^{n-1}\over\sqrt2}$. -/
+@[imo_problem_subject combinatorics]
+theorem imo_1979_p6
+    (A E : Fin 8)
+    (hE : E = A + 4)
+    (a : ℕ → ℕ)
+    (ha_def : ∀ n, a n =
+      Fintype.card {p : Fin n → ℤˣ // A + ∑ i, (p i : Fin 8) = E})
+    (n : ℕ) (hn : 0 < n) :
+    a (2*n - 1 : ℕ) = 0 ∧
+    a (2 * n) = (2 + √2)^(n - 1 : ℕ) / √2 - (2 - √2)^(n - 1 : ℕ) / √2 := by
+  exact ⟨imo_1979_p6.parts.odd A E hE a ha_def n hn, imo_1979_p6.parts.even A E hE a ha_def n hn⟩
